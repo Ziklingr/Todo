ea0076c857fecfcab9d6e7d08d9d351375648337
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-10-09 17:00:21 +0300

    message

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  Text,
+  SafeAreaView,
+  TextInput,
+  StatusBar,
+} from 'react-native';
+import Contacts from "expo-contacts";
+
+const STORAGE_KEY = 'todos';
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [Todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getData(); 
+  }, []);
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const addTodo = () => {
+    const newKey = String(Todos.length);
+    const object = { key: newKey, description: newTodo };
+    const newTodos = [...Todos, object];
+    storeData(newTodos);
+    setTodos(newTodos);
+    setNewTodo('');
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(STORAGE_KEY);
+      if (jsonValue !== null) {
+        setTodos(JSON.parse(jsonValue));
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.heading}>Todo List</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Add Todo"
+        value={newTodo}
+        onChangeText={(text) => setNewTodo(text)}
+        returnKeyType="done"
+        onSubmitEditing={addTodo}
+      />
+      <FlatList
+        style={styles.list}
+        data={Todos}
+        extraData={Todos}
+        renderItem={({ item }) => <Text>{item.description}</Text>}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +74,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: '#F0F0F0',
+    borderColor: '#FAFAFA',
+    height: 40,
+    margin: 8,
+  },
+  list: {
+    margin: 8,
   },
 });
